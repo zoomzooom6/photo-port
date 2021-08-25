935a2e55a5e14313016906711b7a852b42062c62
zoomzoom6 <tonyhuang107@live.ca>
2021-08-25 15:37:32 -0400

    create tests for About section

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    //renders About test 1
+    it('renders', () => {
+        render(<About />);
+    });
+
+    //test 2
+    it('matches snapshot DOM node structure', () => {
+        //render About
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
